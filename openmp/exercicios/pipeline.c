56f455d72d101dfce58801c497d72f46f522a3fd
Geovane Fedrecheski <geonnave@gmail.com>
2016-11-28 17:37:18 -0200

    trying to make pipeline with semaphore work..

--- openmp/exercicios/pipeline.c
+++ openmp/exercicios/pipeline.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <semaphore.h>
 #include <omp.h>
 
-#define N 10
+#define N 30
 
 int main()
 {
     int A[N][N], B[N][N], C[N][N], X[N][N], Y[N][N], Z[N][N];
     int i, j;
+    sem_t sem1, sem2;
 
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
@@ -16,29 +18,63 @@ int main()
         }
     }
 
-    #pragma omp parallel num_threads(2) shared(A, B, C, X, Y, Z) private(i, j)
+    sem_init(&sem1, 0, 0);
+    sem_init(&sem2, 0, 0);
+
+    omp_set_num_threads(4);
+    #pragma omp parallel shared(sem1, sem2, A, B, C, X, Y, Z) private(i, j)
     {
         printf("Thread nº %d\n", omp_get_thread_num());
 
-        #pragma omp sections // pipeline #1
+        #pragma omp sections
         {
+
             #pragma omp section
-                for (i = 0; i < N; i++)
+            {
+                printf("before wait z \t");
+                sem_wait(&sem1);
+                sem_wait(&sem2);
+                printf("before wait z \t");
+                for (i = 0; i < N; i++) {
+                    printf("wait z \t");
+                    sem_wait(&sem1);
+                    sem_wait(&sem2);
+                    printf("Thread nº %d\n", omp_get_thread_num());
+                    for (j = 0; j < N; j++)
+                        Z[i][j] = X[i][j] + Y[i][j];
+                    sem_post(&sem1);
+                    sem_post(&sem2);
+                }
+            }
+
+            #pragma omp section
+            {
+                sem_post(&sem1);
+                for (i = 0; i < N; i++) {
+                    printf("wait x \t");
+                    sem_wait(&sem1);
+                    printf("Thread nº %d\n", omp_get_thread_num());
                     for (j = 0; j < N; j++)
                         X[i][j] = A[i][j] * B[i][j];
+                    sem_post(&sem1);
+                }
+                sem_post(&sem1);
+            }
 
             #pragma omp section
-                for (i = 0; i < N; i++)
+            {
+                sem_post(&sem2);
+                for (i = 0; i < N; i++) {
+                    printf("wait y \t");
+                    sem_wait(&sem2);
+                    printf("Thread nº %d\n", omp_get_thread_num());
                     for (j = 0; j < N; j++)
                         Y[i][j] = C[i][j] * -1;
+                    sem_post(&sem2);
+                }
+                sem_post(&sem2);
+            }
         }
-
-        #pragma omp barrier // wait for #1 to finish
-
-        #pragma omp for // pipeline #2
-        for (i = 0; i < N; i++)
-            for (j = 0; j < N; j++)
-                Z[i][j] = X[i][j] + Y[i][j];
     }
 
     printf("\n");
